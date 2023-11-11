20cb71a445e237fee9053b8590dc828614d148e9
liamstanley <liam.stanley@gmail.com>
2023-11-11 13:21:55 +0000

    Added in current hour for the colour changes (not yet built) but functionality for the save button and saving / reloading from local storage is working

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 
 // Using day.js to pull in todays date
 
-var currentday = dayjs().format('DD/MM/YYYY')
+var currentDay = dayjs().format('DD/MM/YYYY')
 
-console.log(currentday);
+console.log(currentDay);
 
 
 // Display currentday in the header
 
-document.getElementById('currentDay').textContent = "Today is " + currentday;
+document.getElementById('currentDay').textContent = "Today is " + currentDay;
+
+
+// Using day.js to pull in the hour 
+
+var currentHour = dayjs().hour();
+
+console.log(currentHour);
+
+
+// Adding function to saveBtn. Event handler + collecting text and hour 
+
+$('.saveBtn').on('click', function() {
+    var text = $(this).siblings('textarea').val();
+    var hour = $(this).siblings('.hour').text();
+
+// Save in local storage, linking hour and text together
+
+localStorage.setItem(hour, text);
+
+console.log("textarea")
+
+});
+
+// Load data from local storage 
+
+function LoadSchedule() {
+    $('.time-block').each(function () {
+        var hour = $(this).find('.hour').text();
+        var text = localStorage.getItem(hour);
+
+        if (text) {
+            $(this).find('textarea').val(text);
+        }
+    })
+}
+
+// Call the function! 
+
+LoadSchedule();
 
